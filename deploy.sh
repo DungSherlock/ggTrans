fd056e772062e92be9f4164f62ec1b611245e594
Huệ Dũng <dungsherlock@gmail.com>
2023-10-27 12:43:20 +0700

    final

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,9 @@ git commit -m """$commitVar""" --allow-empty
 git push
 
 gitCommit=`git rev-parse HEAD`
-link="https://raw.githubusercontent.com/DungSherlock/ggTrans/${gitCommit}/source.txt"
+link="https://raw-githubusercontent-com.translate.goog/DungSherlock/ggTrans/${gitCommit}/source.txt?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp"
 
 echo '[InternetShortcut]\
 URL='$link > ggTrans.url
 open -a "Microsoft Edge" ggTrans.url
 exit
-
