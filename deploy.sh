6d649b6f6655e3b1a61ac09d9a45f4190730c832
Huệ Dũng <dungsherlock@gmail.com>
2023-10-27 12:33:31 +0700

    new

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p 'Noi dung commit: ' commitVar
+git add -A .
+git commit -m """$commitVar""" --allow-empty
+git push
+
+gitCommit=`git rev-parse HEAD`
+link=`https://raw-githubusercontent-com.translate.goog/DungSherlock/eBay/`$gitCommit`/ggTrans?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp`
+
+echo '[InternetShortcut]\
+URL='$link > ggTrans.url
+open -a "Microsoft Edge" ggTrans.url
+exit
