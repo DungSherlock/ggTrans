e1512833220609b63c43b67e92bbca77bfeca203
Huệ Dũng <dungsherlock@gmail.com>
2023-10-30 15:43:33 +0700

    ggTrans

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ for file in *.vtt; do
     if [ -e "$file" ]; then
         link="https://raw-githubusercontent-com.translate.goog/DungSherlock/ggTrans/${gitCommit}/${file}?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp"
         echo '[InternetShortcut]\
-        URL='$link > ${file}.url
-        open -a "Microsoft Edge" ${file}.url
-        open -a "Microsoft Edge" ${file}.url
+        URL='$link > $file.url
+        open -a "Microsoft Edge" $file.url
+        open -a "Microsoft Edge" $file.url
     fi
 done
