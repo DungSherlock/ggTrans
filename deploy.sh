e2fb8b7e14bf9bcf86ff4a5a922bc84c2371b38e
Huệ Dũng <dungsherlock@gmail.com>
2023-10-30 15:42:32 +0700

    ggTrans

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,15 @@ git commit -m "ggTrans" --allow-empty
 git push
 
 gitCommit=`git rev-parse HEAD`
-link="https://raw-githubusercontent-com.translate.goog/DungSherlock/ggTrans/${gitCommit}/source.txt?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp"
+# link="https://raw-githubusercontent-com.translate.goog/DungSherlock/ggTrans/${gitCommit}/source.txt?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp"
 
-echo '[InternetShortcut]\
-URL='$link > ggTrans.url
-open -a "Microsoft Edge" ggTrans.url
-exit
+
+for file in *.vtt; do
+    if [ -e "$file" ]; then
+        link="https://raw-githubusercontent-com.translate.goog/DungSherlock/ggTrans/${gitCommit}/${file}?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp"
+        echo '[InternetShortcut]\
+        URL='$link > ${file}.url
+        open -a "Microsoft Edge" ${file}.url
+        open -a "Microsoft Edge" ${file}.url
+    fi
+done
