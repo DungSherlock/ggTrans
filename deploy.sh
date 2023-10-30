4cc85af3f8ade25763be94293537667844a76147
Huệ Dũng <dungsherlock@gmail.com>
2023-10-30 16:34:11 +0700

    ggTrans

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 # read -p 'Noi dung commit: ' commitVar
+
+for file in *.vtt; do
+    if [ -e "$file" ]; then
+        # Lấy tên tệp (không bao gồm phần mở rộng)
+        fileName="${file%.*}"
+        # Loại bỏ dấu cách và ký tự đặc biệt bằng cách thay thế chúng bằng dấu gạch dưới
+        newFile=$(echo "${fileName}" | sed -e 's/[^A-Za-z0-9._-]/_/g')
+        # Đổi tên tệp .vtt thành tên mới
+        mv "${file}" "$newFile.vtt"
+    fi
+done
+
 git add -A .
 git commit -m "ggTrans" --allow-empty
 git push
-
 gitCommit=`git rev-parse HEAD`
-# link="https://raw-githubusercontent-com.translate.goog/DungSherlock/ggTrans/${gitCommit}/source.txt?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp"
 
 
 for file in *.vtt; do
     if [ -e "$file" ]; then
         link="https://raw-githubusercontent-com.translate.goog/DungSherlock/ggTrans/${gitCommit}/${file}?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp"
-        echo '[InternetShortcut]\
-        URL='$link > $file.url
-        open -a "Microsoft Edge" $file.url
-        open -a "Microsoft Edge" $file.url
+        echo "[InternetShortcut]
+        URL=${link}" > "${file%.*}".url
+        open -a "Microsoft Edge" ${file%.*}.url
     fi
 done
