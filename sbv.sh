aecb670d017c7b4f44480cfd67f1463cad383474
Huệ Dũng <dungsherlock@gmail.com>
2023-11-07 10:28:01 +0700

    ggTrans

--- sbv.sh
+++ sbv.sh
@@ -2,14 +2,14 @@
 
 # read -p 'Noi dung commit: ' commitVar
 
-for file in *.txt; do
+for file in *.sbv; do
     if [ -e "$file" ]; then
         # Lấy tên tệp (không bao gồm phần mở rộng)
         fileName="${file%.*}"
         # Loại bỏ dấu cách và ký tự đặc biệt bằng cách thay thế chúng bằng dấu gạch dưới
         newFile=$(echo "${fileName}" | sed -e 's/[^A-Za-z0-9._-]/_/g')
-        # Đổi tên tệp .txt thành tên mới
-        mv "${file}" "$newFile.txt"
+        # Đổi tên tệp .sbv thành tên mới
+        mv "${file}" "$newFile.sbv"
     fi
 done
 
@@ -19,7 +19,7 @@ git push
 gitCommit=`git rev-parse HEAD`
 
 
-for file in *.txt; do
+for file in *.sbv; do
     if [ -e "$file" ]; then
         link="https://raw-githubusercontent-com.translate.goog/DungSherlock/ggTrans/${gitCommit}/${file}?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp"
         echo "[InternetShortcut]
@@ -30,5 +30,5 @@ done
 
 rm -f *.url
 read -p "Press any key to continue... " -n1 -s
-rm -f *.txt
+rm -f *.sbv
 git push
